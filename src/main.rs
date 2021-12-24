b0edb243f99d486d015ba57a920bb18e2b5351b2
numToStr <vikasraj1911@gmail.com>
2021-12-24 16:52:45 +0530

    feat!: replace `pico-args` with `lexopt`

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,34 @@
 mod cli;
 mod info;
-use std::process::exit;
-use std::process::{Command, Stdio};
 
 use cli::Cli;
+use std::process::{exit, Command, Stdio};
 use zenv::Zenv;
 
-use crate::info::{NAME, VERSION};
+fn bootstrap() -> Result<i32, lexopt::Error> {
+    let cli = Cli::parse()?;
 
-fn bootstrap() -> Result<i32, String> {
-    let args = Cli::parse()?;
+    let path = cli.path.ok_or("-f/--file is required")?;
+    let bin = cli.binary.ok_or("<binary> name is required")?;
 
-    if args.version {
-        println!("{} {}", NAME, VERSION);
-        return Ok(0);
-    }
-
-    if args.help {
-        print!("{}", Cli::help_doc());
-        return Ok(0);
-    }
-
-    let fpath = args.path()?;
-
-    let binary = args.binary()?;
-
-    let vars = Zenv::new(fpath, args.expand)
+    let vars = Zenv::new(&path, cli.expand)
         .parse()
         .map_err(|e| e.to_string())?;
 
-    let mut program = Command::new(&binary)
-        .args(&args.bin_args)
+    let mut program = Command::new(&bin)
+        .args(&cli.args)
         .stdin(Stdio::inherit())
         .stdout(Stdio::inherit())
         .stderr(Stdio::inherit())
         .envs(vars)
         .spawn()
-        .map_err(|_| format!("Unable to spawn program - `{}`", binary.to_str().unwrap()))?;
+        .map_err(|_| "Unable to spawn program!")?;
 
     let code = program
         .wait()
         .map_err(|e| e.to_string())?
         .code()
-        .ok_or("Failed to grab the exit code")?;
+        .ok_or("Failed to grab the exit code!")?;
 
     Ok(code)
 }
@@ -51,7 +37,7 @@ fn main() {
     match bootstrap() {
         Ok(code) => exit(code),
         Err(e) => {
-            eprintln!("ERROR :: {}", e);
+            eprintln!("{}", e);
             exit(1)
         }
     };
